4d0c5e80225fde7a29678a17c4a54ae78f2bb412
Nate Miller <natekmiller96@gmail.com>
2018-06-22 21:54:42 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,13 +1,7 @@
+
 def reverse_each_word(string)
   array = string.split
-  reverse_array = []
-  array.each do |word|
-    reverse_array << word.reverse
+  array.collect do |word|
+    array << word.reverse
   end
-  reverse_string = ""
-  reverse_array.each do |word|
-    reverse_string << "#{word} "
-  end
-  reverse_string = reverse_string.strip
-  return reverse_string
 end
